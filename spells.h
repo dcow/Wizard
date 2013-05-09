e9267d6a20eed4cf82ceb20824bddf62213612ad
David Cowden <dcow90@gmail.com>
2013-05-09 15:23:48 -0500

    add gesture to spell mapping

---
+++ spells.h
@@ -0,0 +1,15 @@
+/*
+ * WIZARD Gesture to Spell Map
+ * Author: David Cowden
+ */
+
+#inlcude "glove-input.h"
+
+// Map spells to gestures..
+#define SPL_SHIELD  GST_PALM   
+#define SPL_NONE    GST_OKAY   
+#define SPL_HEAL    GST_MIND  
+#define SPL_DRAIN   GST_SPIDY 
+#define SPL_FIREB   GST_POINT 
+#define SPL_REFLECT GST_TEA   
+#define SPL_CHARGE  GST_FIST 
